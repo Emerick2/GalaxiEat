82f6ba6a40efdd525d3c82ff1a732ab6107b7d57
Emerick <pacaudemerick@gmail.com>
2026-02-20 09:51:57 +0100

    Ajout de la jointure

--- Inserer-de-la-donnee.sql
+++ Inserer-de-la-donnee.sql
@@ -4,5 +4,5 @@ VALUES ("Milky Way Diner", "Terre");
 INSERT INTO Dishes ("Name", "Price", "Category")
 VALUES ("Burger d’Astéroïde", 12.5, "Burgers");
 
-INSERT INTO Employees ("Firstname", "Role")
-VALUES ("Zorglub", "Chef cuisinier");
+INSERT INTO Employees ("Firstname", "Lastname", "Role", "IdRestaurant")
+VALUES ("Zorglub", "Bidoum", "Chef cuisinier", 1);
---
+++ Jointure.sql
@@ -0,0 +1,20 @@
+--  Liste des employés avec le nom de leur restaurant. 
+SELECT Employees."Firstname" ||  ' ' || Employees."Lastname" AS "Nom de l'employé", Restaurants."Name" AS "Nom du restaurent"
+FROM Employees
+JOIN Restaurants ON Restaurants."IdRestaurant" = Employees."IdRestaurant";
+
+-- Liste des plats commandés avec : (name (Dishes) / (name of customers) /  planète (Restaurant)) 
+SELECT DISTINCT Dishes."Name" AS "Dishes", CustomerOrders."Customer_name" AS "name of customers", Restaurants."Name" || ' - ' || Restaurants."Planet" AS "Restaurant"
+FROM OrderItems
+JOIN Dishes ON Dishes."IdDishes" = OrderItems."IdDishes"
+JOIN CustomerOrders ON CustomerOrders."IdOrders" = OrderItems."IdOrders"
+JOIN Restaurants ON Restaurants."IdRestaurant" = CustomerOrders."IdRestaurant";
+
+
+
+-- Lister tous les restaurants avec leur nombre d’employés.
+
+SELECT COUNT(Employees."IdRestaurant" = Restaurants."IdRestaurant") AS "nombre d’employés", Restaurants."Name" AS "Nom du restaurant"
+FROM Restaurants
+JOIN Employees ON Employees."IdRestaurant" = Restaurants."IdRestaurant"
+GROUP BY Restaurants."IdRestaurant";
