4e30f0998dbf666a0940da7ccb56e9385f95c208
Emerick <pacaudemerick@gmail.com>
2026-02-20 09:10:34 +0100

    Ajout de la partie d'insertion de donnée

---
+++ Inserer-de-la-donnee.sql
@@ -0,0 +1,8 @@
+INSERT INTO Restaurants ("Name", "Planet")
+VALUES ("Milky Way Diner", "Terre");
+
+INSERT INTO Dishes ("Name", "Price", "Category")
+VALUES ("Burger d’Astéroïde", 12.5, "Burgers");
+
+INSERT INTO Employees ("Firstname", "Role")
+VALUES ("Zorglub", "Chef cuisinier");
