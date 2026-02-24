a1dd70ed18e6e1bd25947808884a6d266772c1c1
pomaxime <maxime.poyet@ynov.com>
2026-02-24 08:40:06 +0100

    first request program

---
+++ first_request.sql
@@ -0,0 +1,23 @@
+-- Lister tous les restaurants de la galaxie.
+SELECT Name 
+FROM Restaurants;
+
+-- Lister tous les plats classés par prix décroissant.
+SELECT Name, Price
+FROM Dishes
+ORDER BY Price DESC;
+
+-- Lister tous les employés triés par rôle.
+SELECT FirstName, LastName, Role
+FROM Employees
+ORDER BY Role;
+
+-- Lister tous les plats végétariens
+SELECT Name
+FROM Dishes
+WHERE is_vegan = TRUE;
+
+-- Lister les plats dont le prix est supérieur à la moyenne des prix.
+SELECT Name, Price
+FROM Dishes
+WHERE Price > (SELECT AVG(Price) FROM Dishes);
