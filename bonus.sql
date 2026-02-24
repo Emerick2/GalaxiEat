34ec37d80d6dfc5477d2036902ba7f80a3587cb5
Emerick <pacaudemerick@gmail.com>
2026-02-24 09:54:40 +0100

    Correction

--- bonus.sql
+++ bonus.sql
@@ -3,15 +3,19 @@ CREATE TABLE Ingredients (
 	IdIngredient INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	Name VARCHAR(50)
 	is_vegan BOOLEAN;
-)
+);
 
 -- relation DishIngredients 
-
+CREATE TABLE DishIngredients (
+	IdDishIngredients INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	IdIngredient INTEGER NOT NULL,
+	IdDish INTEGER NOT NULL
+);
 
 -- Générer un "menu vegan"
 SELECT *
 FROM Dishes
-WHERE Dishes."is_vegan" IS NOT NULL;
+WHERE Dishes."is_vegan" = "True";
 
 -- Créer un classement des restaurants par ancienneté 
 SELECT *
